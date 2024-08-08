607ec0afc307d0836556efc14054dbcdef8abaed
Adil Javed <adiljavaid125@gmail.com>
2024-08-08 16:22:09 +0500

    pairs and vectors

---
+++ pair.cpp
@@ -0,0 +1,20 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     pair <int,int> p[] = {{1,2},{3,4}};
+//     cout<<p[1].second<<endl;
+// }
+// ====================Vectors/iterator======================
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+vector<int> v = {1,2,3,4,5};
+// iterator in for loop :
+//1. for(vector<int>::iterator i = v.begin();i!=v.end();i++) 
+//2. for(auto i = v.begin();i != v.end();i++){
+// the auto keyword will automatically assign data type    
+// cout<<*(i)<<endl;
+
+// }
+return 0;
+}
