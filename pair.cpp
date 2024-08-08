29dcaded5aa191df8ba8b75fad072b3267a2bda6
Adil Javed <adiljavaid125@gmail.com>
2024-08-08 16:31:22 +0500

    pairs and vectors

--- pair.cpp
+++ pair.cpp
@@ -16,5 +16,15 @@ vector<int> v = {1,2,3,4,5};
 // cout<<*(i)<<endl;
 
 // }
+// erase and insert element in vector
+// v.erase(v.begin()+1,v.end()); //output : remove element from 2 to 5
+// v.insert(v.begin(),5);//output:5 1
+// v.insert(v.begin(),5,5);//at 0th position add five occurance of 5 output:5 5 5 5 5 5 1
+// vector <int>copy = v;
+// v.insert(v.begin(),copy.begin(),copy.end());// add an entire vector into another vector
+// for(auto i = v.begin();i != v.end();i++){
+//     cout<<*i;
+// }
+
 return 0;
 }
